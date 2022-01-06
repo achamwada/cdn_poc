d07f1c689716e85c12edc75626d4c1e34a8c346b
Alexander Chamwada <alexander.chamwada@vodafone.com>
2022-01-06 12:39:22 +0000

    Initial commit

---
+++ test/cdn_poc.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as CdnPoc from '../lib/cdn_poc-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdn_poc-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdnPoc.CdnPocStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
