3c7efd06b1ea9c802c17d95a2f93236757e411c3
Alexander Chamwada <alexander.chamwada@vodafone.com>
2022-01-06 14:17:47 +0000

    adding second origin

--- lib/cdn_poc-stack.ts
+++ lib/cdn_poc-stack.ts
@@ -1,15 +1,34 @@
-import * as cdk from '@aws-cdk/core';
-// import * as sqs from '@aws-cdk/aws-sqs';
-
-export class CdnPocStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+import { App, Stack, StackProps } from '@aws-cdk/core';
+import { OriginAccessIdentity, Distribution } from '@aws-cdk/aws-cloudfront';
+import { Bucket } from '@aws-cdk/aws-s3';
+import * as origins from '@aws-cdk/aws-cloudfront-origins';
+export class CdnPocStack extends Stack {
+  constructor(scope: App, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const testBucket = Bucket.fromBucketName(this, 'GreatMercatoBucket', 'greatmercato.com');
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdnPocQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Create Origin Access Identity to be use Canonical User Id in S3 bucket policy
+    const originAccessIdentity = new OriginAccessIdentity(this, 'OAI', {
+      comment: "Created_by_Alexander"
+    });
+
+
+
+    const distribution = new Distribution(this, 'GreatMercatoDistribution', {
+      defaultBehavior: {
+        origin: new origins.S3Origin(testBucket, {
+          originPath: "/assets",
+          originAccessIdentity
+        }),
+
+      }
+    })
+    const dealexbucket = Bucket.fromBucketName(this, 'DeAlexBucket', 'dealexbucket');
+
+    distribution.addBehavior("free/", new origins.S3Origin(dealexbucket, {
+      originPath: "/free",
+      originAccessIdentity
+    }))
   }
 }
